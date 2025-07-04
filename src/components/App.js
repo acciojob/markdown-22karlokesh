aeb086f52ebda7f2f5e54a2332585d5a7d9888b7
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-04 12:42:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,24 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import MarkdownEditor from "./MarkdownEditor";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
 
